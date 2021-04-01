6b2920bd44445e3d39c333f2b948457e80ac7e50
Jihwan Ahn <k1tstw@tutanota.com>
2021-04-01 09:19:27 +0900

    Map / Drawer

---
+++ Map.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <vector>
+
+class Map
+{
+public:
+    
+    Map(int size);
+
+    void SetSize(int newSize);
+    int GetSize();
+
+    void ResetMap();
+  
+    void SetValue(int y, int x, int val);
+
+    int GetValue(int y, int x);
+
+    void RandomSpawn();
+
+    int MoveLeft();
+    int MoveRight();
+    int MoveUp();
+    int MoveDown();
+
+    bool operator != (Map& otherMap);
+
+    void Print();
+
+private:
+    std::vector<std::vector<int>> map;
+
+    int size;
+};
